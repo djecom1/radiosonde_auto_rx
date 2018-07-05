05aace49121dceb1957ac5c8149645a834a50582
djecom1 <30767830+djecom1@users.noreply.github.com>
2018-07-05 10:04:57 +0200

    Update build.sh

--- auto_rx/build.sh
+++ auto_rx/build.sh
@@ -8,13 +8,16 @@ echo "Building rs_detect"
 cd ../scan/
 gcc rs_detect.c -lm -o rs_detect
 
-echo "Building RS92/RS41/DFM Demodulators"
+echo "Building RS92/RS41/DFM/M10 Demodulators (mod by F4GMU)"
 cd ../demod/
 gcc -c demod.c
 gcc -c demod_dft.c
 gcc rs92dm_dft.c demod_dft.o -lm -o rs92ecc -I../ecc/ -I../rs92
 gcc rs41dm_dft.c demod_dft.o -lm -o rs41ecc -I../ecc/ -I../rs41
 gcc dfm09dm_dft.c demod_dft.o -lm -o dfm09ecc -I../ecc/ -I../dfm
+cd ../m10/
+gcc -o m10ecc m10gtop.c -lm
+chmod +x m10ecc
 
 # Copy all necessary files into this directory.
 echo "Copying files into auto_rx directory."
@@ -23,5 +26,6 @@ cp ../scan/rs_detect .
 cp ../demod/rs92ecc .
 cp ../demod/rs41ecc .
 cp ../demod/dfm09ecc .
+cp ../m10/m10ecc .
 
 echo "Done!"
